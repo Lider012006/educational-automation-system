5a57d12a8f3f06a02cda914fb568cb7a43722c69
Lider012006 <zuhramuminova547@gmail.com>
2026-09-01 19:42:46 +0600

    Add UserViewSet to users/views.py

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from rest_framework.serializers import ModelSerializer
 
-# Create your views here.
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email']
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
