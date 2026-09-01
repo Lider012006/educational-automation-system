0793b831fe841d050176471ff4bd2576be8c4474
Lider012006 <zuhramuminova547@gmail.com>
2026-09-01 18:56:12 +0600

    Update App.js with new course form routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import CourseDetailPage from './pages/CourseDetailPage';
+import CourseFormPage from './pages/CourseFormPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/courses/new" element={<CourseFormPage />} />
         <Route path="/courses/:id" element={<CourseDetailPage />} />
+        <Route path="/courses/:id/edit" element={<CourseFormPage />} />
       </Routes>
     </Router>
   );
